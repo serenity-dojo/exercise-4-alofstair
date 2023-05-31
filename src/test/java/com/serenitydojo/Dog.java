6197b65b666849ae147c891b4affebecf2318bd5
agoodall <agoodall@workforcesoftware.com>
2023-05-31 17:27:27 +0100

    Try4

--- src/test/java/com/serenitydojo/Dog.java
+++ src/test/java/com/serenitydojo/Dog.java
@@ -7,8 +7,6 @@ public class Dog {
 
     private int age;
 
-    public static final String DOG_NOISE = "Woof";
-
 
     public Dog(String name, String favoriteToy, int age) {
         this.name = name;
@@ -28,8 +26,14 @@ public class Dog {
         return age;
     }
 
-    public void makeNoise() {
-        System.out.println(DOG_NOISE);
+    public void dogSound() {
+        makeNoise();
+
+    }
+
+    public String makeNoise() {
+        System.out.println("Woof");
+        return ("Woof");
     }
 
     public void feed() {
--- src/test/java/com/serenitydojo/WhenCreatingObjects.java
+++ src/test/java/com/serenitydojo/WhenCreatingObjects.java
@@ -16,7 +16,7 @@ public class WhenCreatingObjects {
     public void whenADogBarks() {
         Dog fido = new Dog("Fido","Bone", 5);
         fido.makeNoise();
-        String dogSound = "Woof";
+
         // TODO: implement a method in the Dog sound called makeNoise() that returns the sound a dog makes: "Woof", e.g.
         //dogSound = fido.makeNoise();
 
